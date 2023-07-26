f69b450f62b8377b798e2433854b5be16fb203b5
Web Developer Services <80251513+webdev-service@users.noreply.github.com>
2023-07-26 19:42:36 +0800

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logowide.png';
+import './App.css';
+
+function App() {
+    return (
+
+
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+
+
+    );
+}
+
+export default App;
